bbef817827ac81dfd7a0cbed6c78e3a34bd9ec23
Mrhwick <matt.hardwick@getbellhops.com>
2015-08-15 18:25:26 -0500

    Really simple attempt at trying networking.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate rmp as msgpack;
+
+use std::io;
+use std::io::prelude::*;
+use std::net::{TcpStream, TcpListener};
+use std::thread;
+use std::string::String;
+
+fn handle_client(mut stream: TcpStream) {
+    println!("Connection received.");
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    thread::spawn(move|| {
+        let listener = TcpListener::bind("127.0.0.1:33017").unwrap();
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(move|| {
+                        // connection succeeded
+                        handle_client(stream)
+                    });
+                }
+                Err(e) => {println!("error condition {}", e);}
+            }
+        }
+    });
+
+    let mut stream = TcpStream::connect("127.0.0.1:33017").unwrap();
+    
+    let _ = stream.write(&[1]);
+    
+    let _ = stream.read(&mut [0; 128]);
 }
---
+++ src/message_structures.rs
@@ -0,0 +1,27 @@
+// struct SwarmConfig {
+//     protocol_version: u16,
+//     message_id: u64,
+//     message_type: u16,
+// 	client_version: u64,
+// }
+
+// struct Index {
+//     protocol_version: u16,
+//     message_id: u64,
+//     message_type: u16,
+//     length: u64,
+// }
+
+// struct Request {
+//     protocol_version: u16,
+//     message_id: u64,
+//     message_type: u16,
+//     length: u64,
+// }
+
+// struct Response {
+//     protocol_version: u16,
+//     message_id: u64,
+//     message_type: u16,
+//     length: u64,
+// }
