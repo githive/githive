92f3260dc4695533ef19eca864a15c573b01c618
Mrhwick <matt.hardwick@getbellhops.com>
2015-08-18 16:41:51 -0400

    Sequences RepositoryIndexMessages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rmp as msgpack;
+extern crate time as time;
 
 mod listener;
 mod message_structures;
@@ -27,18 +28,66 @@ fn main() {
     let client_name = String::from("Git Hive");
     let client_version = String::from("0.0.1");
 
+    let repo_path = String::from("/githive/githive-protocol");
+    let repo_path_two = String::from("/githive/githive-client");
+
     let message = message_structures::Message{
-        protocol_name_length: protocol_name.len() as u8,
         protocol_name: protocol_name.into_bytes(),
-        protocol_version_length: protocol_version.len() as u8,
         protocol_version: protocol_version.into_bytes(),
         message_id: 51733 as u16,
         message_type: 0 as u16,
-        real_message: &message_structures::SwarmConfig{
-            client_name_length: client_name.len() as u8,
+        real_message: &message_structures::SwarmConfigurationMessage{
             client_name: client_name.into_bytes(),
-            client_version_length: client_version.len() as u8,
             client_version: client_version.into_bytes(),
+            repositories: vec![
+                message_structures::RepositoryInformation{
+                    path: repo_path.into_bytes(),
+                },
+                message_structures::RepositoryInformation{
+                    path: repo_path_two.into_bytes(),
+                }
+            ],
+        },
+    };
+
+    let _ = stream.write_all(&message.serialize());
+
+    let protocol_name = String::from("Git Hive Protocol");
+    let protocol_version = String::from("0.0.1");
+    let client_name = String::from("Git Hive");
+    let client_version = String::from("0.0.1");
+
+    let directory_one = String::from("/");
+
+    let file_one_name = String::from("README.md");
+
+    let block_hash = String::from("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+
+    let message = message_structures::Message {
+        protocol_name: protocol_name.into_bytes(),
+        protocol_version: protocol_version.into_bytes(),
+        message_id: 51734 as u16,
+        message_type: 1 as u16,
+        real_message: &message_structures::RepositoryIndexMessage{
+            directories: vec![
+                message_structures::DirectoryInformation{
+                    directory_path: directory_one.into_bytes(),
+                    files: vec![
+                        message_structures::FileInformation{
+                            filename: file_one_name.into_bytes(),
+                            modified: time::get_time(),
+                            version: 0 as u32,
+                            local_version: 0 as u32,
+                            blocks: vec![
+                                message_structures::BlockInformation{
+                                    size: 1 as u32,
+                                    hash: block_hash.into_bytes(),
+                                }
+                            ],
+                        }
+                    ],
+                }
+            ],
         },
     };
 
