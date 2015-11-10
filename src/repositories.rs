02e6232fcfa84d7a3905fbeda9a64eab3d2cc535
Mrhwick <matt.hardwick@getbellhops.com>
2015-11-10 17:05:39 -0500

    Use file parsing to get the interesting repos from our data dir.

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,5 +1,45 @@
+use std::fs::{DirEntry, read_dir, metadata};
+use std::path::Path;
 
 use errors::Error;
+
+pub fn parse_owners_with_repos_from_folder(folder_path: &Path) -> Result<Vec<OwnerTree>, Error> {
+
+		let mut owners = vec![];
+
+		let owner_dir_list = try!(read_dir(folder_path));
+
+		for owner_entry in owner_dir_list {
+
+			let owner_entry = try!(owner_entry);
+
+			if try!(metadata(owner_entry.path())).is_dir() {
+
+				let mut owner_instance = OwnerTree{
+					owner: String::from(owner_entry.path().file_name().unwrap().to_str().unwrap()),
+					repositories: vec![],
+				};
+
+				let repo_dir_list = try!(read_dir(&owner_entry.path()));
+
+				for repo_entry in repo_dir_list {
+					let repo_entry = try!(repo_entry);
+
+						if try!(metadata(repo_entry.path())).is_dir() {
+
+							try!(owner_instance.add_repo(String::from(repo_entry.path().file_name().unwrap().to_str().unwrap())));
+
+						}
+				}
+
+				owners.push(owner_instance);
+			}
+
+		}
+
+		return Ok(owners);
+}
+
 
 pub struct OwnerTree {
 	pub owner: String,
